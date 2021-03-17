cf50186f05e0e54e6bd3fa74928629862624ce37
lukeown <lukeown@gmail.com>
2021-03-17 16:06:29 -0400

    add bootstrap styles; add card display for search results; add search confirmation;

--- search.js
+++ search.js
@@ -18,10 +18,13 @@
 
 async function searchBooks() {
     event.preventDefault()
+    
     let bookResult = [] // clears out bookResults on every function call
-    document.getElementById('card').innerHTML = '' //clears out the card div for every search
 
     let userInput = document.getElementById('input').value
+
+    document.getElementById('searchConfirm').innerHTML = `Searching for: ${userInput}`
+
     const response =  await fetch(`http://openlibrary.org/search.json?q=${userInput}`)
 
     let book = await response.json()
@@ -34,15 +37,18 @@ async function searchBooks() {
     let bookCoverRequest = await (fetch(`http://covers.openlibrary.org/b/isbn/${book.docs[3].isbn[0]}-M.jpg`)) 
     console.log(bookCoverRequest.url)
     
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < book.length; i++) {
         console.log(bookCoverRequest.url)
     }
 
     let bookCover = bookCoverRequest.url
     console.dir(bookCover)
-    for (let i = 0; i < 5; i++) { // pushes the first 5 search results to the bookResult array
+    console.log(book.numFound)
+    for (let i = 0; i <= 5; i++) { // pushes the first 5 search results to the bookResult array
         bookResult.push({cover: bookCover, title: book.docs[i].title, author: book.docs[i].author_name, date: book.docs[i].first_publish_year, isbn: book.docs[i].isbn[0]})
     }
+    
+    document.getElementById('searchConfirm').innerHTML = `Showing results for: ${userInput}`
 
     console.log(bookResult)
     generateCard(bookResult)
@@ -52,8 +58,39 @@ async function searchBooks() {
 //generateCard takes the books array and throws the information into the 'card' div
 //also creates an img element within the 'card' div, using the url stored bookResult.cover, which is then passed into the generateCard function as bookList.cover
 
+
+
+
+
+
+
+
+
+
+
 function generateCard(bookList) { 
-  for (let i = 0; i < bookList.length; i++) {
-    document.getElementById('card').innerHTML += `<img src='${bookList[i].cover}' /> ${bookList[i].title}, ${bookList[i].author}, ${bookList[i].date} -- ISBN: ${bookList[i].isbn}<br>`
+  for (let i = 0; i < bookList.length; i++) { 
+    //for every book, make a card and fill in the info
+    document.getElementById('card').innerHTML += // fill in card structure and content
+        `
+            <div class="card mb-3" id="card-shell" style="max-width: 600px;">
+                <div class="row g-0">
+                    <div class="col-md-4" id="card-image">
+                        <img src='${bookList[i].cover}' />
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body" id="card-body">
+                            <h5>${bookList[i].title}</h5> 
+                            <p>${bookList[i].author}</p> 
+                            <p>${bookList[i].date}</p>
+                            <p>ISBN: ${bookList[i].isbn}</p>
+                            <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+
+   
 } 
 }
